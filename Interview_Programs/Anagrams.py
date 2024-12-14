28b517f57793f545a3f5ae9bb579bfe9f7cdac3f
Harshal Gangwal <122103358+harshalgangwal@users.noreply.github.com>
2024-12-14 11:10:17 +0530

    Update Anagrams.py

--- Interview_Programs/Anagrams.py
+++ Interview_Programs/Anagrams.py
@@ -9,7 +9,21 @@ def is_anagrams(str1, str2):
     if len(str1)!=len(str2):
         return False
     else:
-        char1 = []
+        string1 = str1.lower()
+        string2 = str2.lower()
+
+        char_array1 = list(string1)
+        char_array2 = list(string2)
+
+        char_array1.sort()
+        char_array2.sort()
+
+        # Compare the sorted arrays
+        
+        if char_array1 == char_array2:
+            return "Strings are anagrams"
+        else:
+            return "Strings are not anagrams"
 
 
 
