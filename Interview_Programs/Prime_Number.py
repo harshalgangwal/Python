c4cb5ba4d63a1e57125b1b2e7eae72169f6e4610
Harshal Gangwal <122103358+harshalgangwal@users.noreply.github.com>
2024-12-14 11:15:13 +0530

    Update Prime_Number.py

--- Interview_Programs/Prime_Number.py
+++ Interview_Programs/Prime_Number.py
@@ -1,20 +1,13 @@
-"""def is_prime(n):
+def is_prime(n):
     if n <= 1:
         return False
     elif n <= 3:
         return True
-    elif n % 2 == 0 and n % 3 == 0:
+    elif n % 2 == 0 or n % 3 == 0:
         return False
     i = 5
-    while """
-"""n = 24
-prime_factors = []
-for i in range(1,n):
-    if n % i == 0:
-        prime_factors.append(i)
-print(prime_factors)"""
-
-
-def is_armstrong(n):
-    sum = 0
-    for i in range
+    while i * i <= 5:
+        if n % i == 0 or n % (i+2) == 0:
+            return False
+        i += 6
+    return True
